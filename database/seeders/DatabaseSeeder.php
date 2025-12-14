b2b4a316f6c7d4086898a25dc2a394e3fb009c93
Marcos7364 <21160618@itoaxaca.edu.mx>
2025-12-13 20:37:28 -0600

    usuario_defecto

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,26 @@
 
 namespace Database\Seeders;
 
+// Asegúrate de importar Hash para encriptar la contraseña
+use Illuminate\Support\Facades\Hash; 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
+        // 1. Llamamos al Seeder de ejercicios (para que no se borren)
+        $this->call([
+            EjerciciosSeeder::class,
+        ]);
+
+        // 2. Creamos TU usuario administrador
         User::factory()->create([
-            'nombre' => 'Test User',
-            'email' => 'test@example.com',
+            'nombre' => 'Administrador',
+            'email' => 'admin@admin.com', // Usa este correo fácil
+            'password' => Hash::make('12345678'), // <--- TU CONTRASEÑA NUEVA
+            'rol' => 'estudiante', // <--- IMPORTANTE para poder crear/editar ejercicios
         ]);
     }
 }
