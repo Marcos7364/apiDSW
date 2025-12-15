b630d55bff574c9fb34befbad20b608721b6b4b2
Marcos7364 <21160618@itoaxaca.edu.mx>
2025-12-14 22:26:49 -0600

    frontyback

--- config/cors.php
+++ config/cors.php
@@ -2,14 +2,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'web-login'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'http://localhost:5173',
-        'http://localhost:5174',
-        'http://127.0.0.1:5173',
-        'http://127.0.0.1:5174',
-        'http://localhost:8000',
-        'http://127.0.0.1:8000'
-    ],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
